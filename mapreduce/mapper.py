1a7d5c5082e76817af7d7452bbcb0f3516fa6640
apoorvumang <apoorvumang@gmail.com>
2018-09-08 14:11:26 +0530

    very light mapper.py (no libraries)

--- mapreduce/mapper.py
+++ mapreduce/mapper.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python
 import re
 import sys
-import nltk
-import contractions
-import inflect
 from collections import Counter
-from bs4 import BeautifulSoup
-from nltk.stem import LancasterStemmer, WordNetLemmatizer
 
-
-def strip_html(text):
-    soup = BeautifulSoup(text, "html.parser")
-    return soup.get_text()
+def remove_till_first_quote(text):
+    regex = r"^(.*?)\""
+    text = re.sub(regex, '', text)
+    return text
 
 def remove_unicode(text):
     """Replace unicode codes like \uxxxx with space"""
@@ -20,14 +15,10 @@ def remove_unicode(text):
     return text
 
 def denoise_text(text):
-    text = strip_html(text)
+    text = remove_till_first_quote(text)
     text = remove_unicode(text)
     text = remove_punctuation(text)
     return text
-
-def replace_contractions(text):
-    """Replace contractions in string of text"""
-    return contractions.fix(text)
 
 def to_lowercase(words):
     """Convert all characters to lowercase from list of tokenized words"""
@@ -43,16 +34,6 @@ def remove_punctuation(text):
     return text
 
 
-
-def stem_words(words):
-    """Stem words in list of tokenized words"""
-    stemmer = LancasterStemmer()
-    stems = []
-    for word in words:
-        stem = stemmer.stem(word)
-        stems.append(stem)
-    return stems
-
 def convert_unicode_to_string(words):
     new_words = []
     for word in words:
@@ -60,19 +41,6 @@ def convert_unicode_to_string(words):
     return new_words
 
 
-
-# document = denoise_text(document)
-# document = replace_contractions(document)
-# words = nltk.word_tokenize(document)
-# words = normalize(words)
-# print("Documents done: ", documentsDoneCount)
-# documentsDoneCount += 1
-# stems = convert_unicode_to_string(stem_words(words))
-# c = Counter(stems)
-# pairList = []
-# for word, count in c.items():
-#     if(word):
-#         pairList.append(word + ':' + str(count))
 
 for line in sys.stdin:
     line = line.strip()
@@ -82,7 +50,6 @@ for line in sys.stdin:
     classes = classes.strip()
     classes = classes.split(',')
     document = denoise_text(document)
-    # words = nltk.word_tokenize(document)
     words = document.split()
     words = to_lowercase(words)
     for myword in words:
